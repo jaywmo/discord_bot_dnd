89a0c5caa5f17cdb78ff79cab62014a737aa7eb9
jaywmo <jwmo@sfu.ca>
2018-03-22 01:54:24 -0700

    roll command

--- Bot/PlayerBot.py
+++ Bot/PlayerBot.py
@@ -3,15 +3,14 @@ from discord.ext import commands
 import asyncio
 import numpy as np
 import pandas as pd
+import random
+import re
 import logging
 import yaml
 
 
 with open("../Config/PlayerBot_config.yaml", 'r') as stream:
-    try:
-        print(yaml.load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+    config = yaml.load(stream)
 
 
 logging.basicConfig(level=logging.INFO)
@@ -19,7 +18,7 @@ logging.basicConfig(level=logging.INFO)
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='/', description=description)
+bot = commands.Bot(command_prefix=config['command_prefix'], description=description)
 
 
 @bot.event
@@ -36,20 +35,32 @@ async def add(left : int, right : int):
     await bot.say(left + right)
 
 
-@bot.command(name='roll', aliases=['r'], brief='Rolls a dice in NdN format.')
+@bot.command(name='roll', aliases=['r'], brief='Rolls a dice in NdN format. Supports addition.',
+             description='Rolls a dice in NdN format. Supports addition per roll or on sum.')
 async def roll(dice : str, *args):
     """Rolls a dice in NdN format."""
     try:
         rolls, limit = map(int, dice.split('d'))
-    except Exception:
+    except ValueError:
         await bot.say('Format has to be in NdN!')
         return
 
-    result = np.random.randint(1, limit, size=rolls).tolist()
+    result = np.random.randint(1, limit + 1, size=rolls)
+    result_sum = np.sum(result)
     logging.info(result)
     # result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say('`{}\n={}`'.format(str(result)[1:-1], np.sum(result)))
-    await bot.say('`{}`'.format(args))
+    if args:
+        regex = r'(\w*\+)(\d+)'
+        match = re.search(regex, args[0])
+        if match:
+            if match.group(1) == '+':
+                result_sum += int(match.group(2))
+            elif match.group(1) == 'r+':
+                result += int(match.group(2))
+                result_sum = np.sum(result)
+
+    await bot.say('`{}\n={}`'.format(str(result.tolist())[1:-1], result_sum))
+    logging.info('`{}`'.format(args))
 
 
 @bot.command(description='For when you wanna settle the score some other way')
